570757b191b625380bc85148befdf7996765f84a
TheRealFirst <88617589+TheRealFirst@users.noreply.github.com>
2023-06-10 21:00:16 +0200

    Windows Platform Layer

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,6 +1,9 @@
 #include <core/logger.h>
 #include <core/asserts.h>
 
+// TODO: Test
+#include <platform/platform.h>
+
 int main(void){
     JFATAL("A test message: %f", 3.14f);
     JERROR("A test message: %f", 3.14f);
@@ -9,7 +12,14 @@ int main(void){
     JDEBUG("A test message: %f", 3.14f);
     JTRACE("A test message: %f", 3.14f);
 
-    JASSERT(1 == 0);
+    platform_state state;
+    if(platform_startup(&state, "J58 Engine Testbed", 100, 100, 1280, 720)){
+        while (TRUE)
+        {
+            platform_pump_messages(&state);
+        }
+    }
+    platform_shutdown(&state);
 
     return 0;
 }
