8f5adf7b51ad893f9a025f168cf33da52ec0d074
TheRealFirst <88617589+TheRealFirst@users.noreply.github.com>
2023-06-12 17:52:47 +0200

    Application Layer and Entry Point

--- engine/src/platform/platform_linux.c
+++ engine/src/platform/platform_linux.c
@@ -186,7 +186,7 @@ b8 platform_pump_messages(platform_state* plat_state) {
         }
 
         // Input events
-        switch (event->response_type & ~0x80) {
+         switch (event->response_type & ~0x80) {
             case XCB_KEY_PRESS:
             case XCB_KEY_RELEASE: {
                 // TODO: Key presses and releases
@@ -194,14 +194,13 @@ b8 platform_pump_messages(platform_state* plat_state) {
             case XCB_BUTTON_PRESS:
             case XCB_BUTTON_RELEASE: {
                 // TODO: Mouse button presses and releases
-            }
-            case XCB_MOTION_NOTIFY:
+            } break;
+            case XCB_MOTION_NOTIFY: {
                 // TODO: mouse movement
-                break;
-
+            } break;
             case XCB_CONFIGURE_NOTIFY: {
                 // TODO: Resizing
-            }
+            } break;
 
             case XCB_CLIENT_MESSAGE: {
                 cm = (xcb_client_message_event_t*)event;
